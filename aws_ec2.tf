5100d76893cbd8a86f746e8fce9a5e31ff83d629
veeresh996 <139729588+veeresh996@users.noreply.github.com>
2023-09-23 06:26:44 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,14 +1,13 @@
 provider "aws"{
-region = "ap-south-1"
-}
-
-
-resource "aws_instance" "AWSServer"{
-ami = "ami-02bb7d8191b50f4bb"
-instance_type = "t2.micro"
-security_groups = ["launch-wizard-2"]
-key_name = "devopsmithuntechnologies"
-tags = {
-Name =  "tomcatservers"
-}
-}
+ region= "ap-south-1"
+ }
+ 
+ resource "aws_instance" "AWSServer"{
+ ami = "ami-ami-067c21fb1979f0b27"
+ instance_type = "t2.micro"
+ security_groups = ["launch-wizard-2"]
+ key_name = don don
+ tags = {
+ Name = "tomcatservers"
+ }
+ }
